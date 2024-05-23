a4d4e5a581fecedbaddf0feca7039c5706478d4e
moaz <moaz.edilbe@gmail.com>
2024-05-23 04:09:46 +0400

    add and get referral

--- ProjectContractFactory.sol
+++ ProjectContractFactory.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts@4.7.3/token/ERC20/IERC20.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "./AlignerNFT.sol";
 
@@ -10,9 +11,11 @@ contract ProjectContract {
     IERC20 public USDT;
     AlignerNFT public NFTContract;
     address owner;
-    address public operatorAddress; // Operator address that can act as owner
+    address public operatorAddress;
     mapping(uint256 => Project) public projects;
     uint256 public projectCounter;
+    mapping(address => address) public referrals; // Mapping to store who referred whom
+    mapping(address => address[]) public referralsByUser; // Mapping to store referrals made by each user
 
     // Structs
     struct Project {
@@ -28,7 +31,7 @@ contract ProjectContract {
         uint256 bidCounter;
         VestingRound[] vestingRounds;
         address operatorAddress;
-        mapping(address => bool) whitelist; // Mapping to store whitelist status for each project
+        mapping(address => bool) whitelist;
     }
     struct VestingRound {
         uint256 roundAmount;
@@ -66,6 +69,7 @@ contract ProjectContract {
         uint256 vestedAmount,
         uint256 timestamp
     );
+    event ReferralRecorded(address indexed referrer, address indexed referee);
 
     // Modifiers
     modifier onlyOwner() {
@@ -112,7 +116,6 @@ contract ProjectContract {
         );
         _;
     }
-    // Modifier to check if the sender is whitelisted for the specific project
     modifier onlyWhitelisted(uint256 projectId) {
         require(
             projects[projectId].whitelist[msg.sender],
@@ -138,7 +141,7 @@ contract ProjectContract {
         owner = msg.sender;
         NFTContract = AlignerNFT(nftAddress);
         USDT = IERC20(_usdtAddress);
-        operatorAddress = _operatorAddress; // Initialize operator address in constructor
+        operatorAddress = _operatorAddress;
     }
 
     // Functions
@@ -159,7 +162,7 @@ contract ProjectContract {
         project.biddingStartDate = _biddingStartDate;
         project.biddingEndDate = _biddingStartDate + _biddingDuration;
         project.biddingActive = true;
-        project.operatorAddress = address(0); // No operator address initially
+        project.operatorAddress = address(0);
         return projectId;
     }
 
@@ -229,50 +232,38 @@ contract ProjectContract {
         address[] memory bidders = new address[](project.bidCounter);
         uint256[] memory allocationSizes = new uint256[](project.bidCounter);
 
-        // Populate arrays with bidder addresses and their allocation sizes
         for (uint256 i = 0; i < project.bidCounter; i++) {
             address bidderAddress = project.bidderAddresses[i];
             bidders[i] = bidderAddress;
             allocationSizes[i] = project.bids[bidderAddress].allocationSize;
         }
 
-        // Use a sorting function to sort in descending order
         selectionSort(bidders, allocationSizes, project.bidCounter);
 
-        // Calculate IWO tokens for each bidder and deduct from vesting rounds
         for (uint256 i = 0; i < project.bidCounter; i++) {
             address bidder = bidders[i];
             uint256 allocationSizeUSDT = project.bids[bidder].allocationSize;
 
-            // Iterate through vesting rounds
             for (uint256 j = 0; j < project.vestingRounds.length; j++) {
                 VestingRound storage round = project.vestingRounds[j];
                 uint256 allocationIWO = allocationSizeUSDT / round.iwoPrice;
-                // Store the original round amount
                 uint256 leftRoundAmount = round.roundAmount - round.bidsAmount;
-
-                // Calculate tokens to deduct from this round
                 uint256 tokensToDeduct = allocationIWO < leftRoundAmount
                     ? allocationIWO
                     : leftRoundAmount;
 
-                // Add tokens to the round and bidder
                 round.bidsAmount += tokensToDeduct;
                 project.bids[bidder].allocationIWOSize += tokensToDeduct;
 
-                // Update the bidder's allocation
                 allocationIWO -= tokensToDeduct;
                 allocationSizeUSDT -= tokensToDeduct * round.iwoPrice;
 
-                // Check if the round is completed
                 if (round.roundAmount - round.bidsAmount <= 0)
                     round.completed = true;
 
-                // Check if the bidder's allocation is fully processed
                 if (allocationSizeUSDT <= 0) break;
             }
 
-            // Transfer IWO tokens to the contract
             uint256 vestedAmount = project.bids[bidder].allocationIWOSize;
             require(
                 IWO.transferFrom(project.owner, address(this), vestedAmount),
@@ -289,12 +280,10 @@ contract ProjectContract {
     ) internal pure {
         for (uint256 i = 0; i < n - 1; i++) {
             uint256 maxIndex = i;
-            // Find the index of the maximum element in the unsorted part
             for (uint256 j = i + 1; j < n; j++)
                 if (allocationSizes[j] > allocationSizes[maxIndex])
                     maxIndex = j;
 
-            // Swap the found maximum element with the first element
             (bidders[i], bidders[maxIndex]) = (bidders[maxIndex], bidders[i]);
             (allocationSizes[i], allocationSizes[maxIndex]) = (
                 allocationSizes[maxIndex],
@@ -304,7 +293,6 @@ contract ProjectContract {
     }
 
     function mintNFT(uint256 projectId) internal {
-        // Mint NFT to successful bidders
         address[] memory bidders = projects[projectId].bidderAddresses;
         for (uint256 i = 0; i < bidders.length; i++) {
             address bidder = bidders[i];
@@ -452,13 +440,13 @@ contract ProjectContract {
     }
 
     struct ClaimingDetails {
-        uint256 allocationSize; // Allocation size for the bid
-        uint256 balance; // Balance available for claiming
-        uint256 date; // Current date
-        bool claimAllowed; // Whether claiming is allowed for this month
+        uint256 allocationSize;
+        uint256 balance;
+        uint256 date;
+        bool claimAllowed;
     }
 
-    uint256 public period = 2 minutes; // One month in seconds
+    uint256 public period = 2 minutes;
 
     function calculateVestedAmount(
         uint256 projectId,
@@ -468,23 +456,16 @@ contract ProjectContract {
         Project storage project = projects[projectId];
         Bid storage bid = project.bids[bidder];
 
-        // Ensure the bid exists
         require(bid.timestamp > 0, "Bid not found");
 
-        // Calculate the monthly vesting amount
         uint256 monthlyVestingAmount = bid.allocationIWOSize /
             bid.vestingLength;
 
-        // Calculate the vested amount for the specified month
         uint256 vestedAmount = monthlyVestingAmount * month;
 
-        // Ensure vested amount does not exceed the total allocation
         if (vestedAmount > bid.allocationIWOSize) {
             vestedAmount = bid.allocationIWOSize;
         }
-
-        // Subtract any previously locked amount
-        // vestedAmount -= bid.lockedIWOSize;
 
         return vestedAmount;
     }
@@ -513,7 +494,6 @@ contract ProjectContract {
             uint256 balance = vestedAmount - claimedAmount;
             claimedAmount = vestedAmount;
 
-            // Check if claiming is allowed for this month
             bool claimAllowed = balance > 0 &&
                 currentPeriod >= project.biddingEndDate + ((i + 1) * period);
 
@@ -547,7 +527,6 @@ contract ProjectContract {
             currentMonth
         );
 
-        // Check if the user has any new vested amount to withdraw
         require(
             vestedAmount > 0 && vestedAmount > bid.lockedIWOSize,
             "No new vested amount to withdraw"
@@ -582,7 +561,27 @@ contract ProjectContract {
         projects[projectId].whitelist[_address] = false;
     }
 
-    // Modify functions
+    // Record referral
+    function addReferral(address referrer, address referee) public {
+        require(referrals[referee] == address(0), "Referee already referred");
+        require(referrer != referee, "Cannot refer yourself");
+        referrals[referee] = referrer;
+        referralsByUser[referrer].push(referee);
+        emit ReferralRecorded(referrer, referee);
+    }
+
+    function getReferrer(address referee) external view returns (address) {
+        return referrals[referee];
+    }
+
+    function getReferralsByUser(address referrer)
+        external
+        view
+        returns (address[] memory)
+    {
+        return referralsByUser[referrer];
+    }
+
     function updateProjectDetails(
         uint256 projectId,
         string memory newProjectName,
